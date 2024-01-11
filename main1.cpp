9c35ed7ef371d66226be1184e74c8db2506425b2
Vig Bendegúz <bendi@parphis.hu>
2024-01-11 19:36:22 +0100

    mini talk

--- main1.cpp
+++ main1.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <iostream> 
+#include <string>
 
 int main(int argc, char* argv []) {
-	std::cout << "Hello világ!\n";
+	std::string str;
+	std::cout << "Put your name here!" <<std::endl;
+	std::cin >> str;
+	std::cout << "Hello, " << str << "!\n";
+	std::string answr;
+	std::cout << "How are you?" <<std::endl;
+	std::cin >> answr;
+	if (answr=="fine") {
+		std::cout << "It's super!" <<std::endl;
+	}
+	else if (answr=="bad") {
+		std::cout << "Oh, it isn't good..." <<std::endl;
+	}
+	else {
+		std::cout << "I don't understand your answer..." <<std::endl;
+	}
 	return -1;
 }
